4ba5a621d0da8152d5bf967e4a78b06dee4ef820
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2023-09-23 21:36:56 +0200

    Task 3

--- file.js
+++ file.js
@@ -23,23 +23,88 @@
         Внесите правки в функцию sum. Сделайте так, чтобы при отсутствии одного из параметров, функция выбрасывала ошибку.        
         */
 
-        function sum(a, b) {
-            if(!a) throw new Error("Значення параметра a обов'язкове");
-            if(!b) throw new Error("Значення параметра b обов'язкове");
+        // function sum(a, b) {
+        //     if(!a) throw new Error("Значення параметра a обов'язкове");
+        //     if(!b) throw new Error("Значення параметра b обов'язкове");
 
-            return a + b;
+        //     return a + b;
+        // }
+
+        // try {
+        //     let result = sum(1, 5);
+        //     console.log(result);
+
+        //     result = sum(10);
+        //     console.log(result);
+        // } catch (error) {
+        //     console.error(error.message);
+        // }
+
+//////////////////////////////////////////////////
+
+        /*
+        Задание 3:
+        Добавьте в данный сценарий обработку исключений для той части кода, где исключение может возникнуть.
+        */
+        class Rect {
+            constructor(width, height) {
+                this.width = width;
+                this.height = height;
+            }
+
+            get width() {
+                if (this._width === undefined) {
+                    this._width = 0;
+                }
+
+                return this._width;
+            }
+
+            set width(value) {
+                if (value < 0) {
+                    throw new Error("Значение для width не может быть меньше 0");
+                } else {
+                    this._width = value;
+                }
+            }
+
+            get height() {
+                if (this._height === undefined) {
+                    this._height = 0;
+                }
+
+                return this._height;
+            }
+
+            set height(value) {
+                if (value < 0) {
+                    throw new Error("Значение для height не может быть меньше 0");
+                } else {
+                    this._height = value;
+                }
+            }
+
+            get area() {
+                return this.height * this.width;
+            }
         }
 
-        try {
-            let result = sum(1, 5);
-            console.log(result);
+        let r1 = new Rect(10, 20);
 
-            result = sum(10);
-            console.log(result);
-        } catch (error) {
+        try{
+            r1.height = -10; // ошибка
+            r1.width = 30;
+        } catch(error){
             console.error(error.message);
         }
 
-//////////////////////////////////////////////////
+        console.log("width " + r1.width);
+        console.log("height " + r1.height);
+        console.log("area " + r1.area);
+    
+
+// TODO:
+        // в Task 3 я нічого не писав. потріно створити set area() і помістити throw
+    
 
     
