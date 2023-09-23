e578c2474f6569b79f9a4e615b7b070547f93b16
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2023-09-23 08:38:08 +0200

    Task 2

--- file.js
+++ file.js
@@ -5,12 +5,41 @@
         выводилось сообщение - "Ошибка при чтении JSON"
         */
 
-        let json = '{"name" : "Ivan", "age" : "20"}';
+        // let json = '{"name" : "Ivan", "age" : "20"}';
 
-        try{
-            let obj = JSON.parse(json);
-            console.log(obj.name);
-            console.log(obj.age);
-        } catch(error) {
-            console.error('Ошибка при чтении JSON');
+        // try{
+        //     let obj = JSON.parse(json);
+        //     console.log(obj.name);
+        //     console.log(obj.age);
+        // } catch(error) {
+        //     console.error('Ошибка при чтении JSON');
+        // }
+
+//////////////////////////////////////////////////
+
+
+        /*
+        Задание 2:
+        Внесите правки в функцию sum. Сделайте так, чтобы при отсутствии одного из параметров, функция выбрасывала ошибку.        
+        */
+
+        function sum(a, b) {
+            if(!a) throw new Error("Значення параметра a обов'язкове");
+            if(!b) throw new Error("Значення параметра b обов'язкове");
+
+            return a + b;
         }
+
+        try {
+            let result = sum(1, 5);
+            console.log(result);
+
+            result = sum(10);
+            console.log(result);
+        } catch (error) {
+            console.error(error.message);
+        }
+
+//////////////////////////////////////////////////
+
+    
